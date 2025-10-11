ec9cdf5874d812dc730b091ec01860f56a2245b4
Tai Hunt <Bassin@bassineveryday.com>
2025-10-11 00:00:17 -0600

    Bundle DBM officer images and add local officers module; prefer local images in ClubScreen

---
+++ assets/imports/dbm-officers.local.ts
@@ -0,0 +1,62 @@
+// Local copy of DBM officers that references bundled images in assets/images/
+// This file was generated automatically from the scraped officers JSON and local images.
+const officers = [
+  {
+    name: 'Jeremiah Hofstetter',
+    role: 'President',
+    phone: null,
+    email: null,
+    image: require('../images/jeremiah_hofstetter.jpg'),
+  },
+  {
+    name: 'Bobby Martin',
+    role: 'Vice President',
+    phone: null,
+    email: null,
+    image: require('../images/bobby_martin.jpg'),
+  },
+  {
+    name: 'Tai Hunt',
+    role: 'Secretary',
+    phone: null,
+    email: 'secretary@denverbassmasters.com',
+    image: require('../images/tai_hunt.jpg'),
+  },
+  {
+    name: 'Gordon Phair',
+    role: 'Treasurer',
+    phone: null,
+    email: null,
+    image: require('../images/gordon_phair.jpg'),
+  },
+  {
+    name: 'Howard Binkley',
+    role: 'Tournament Director',
+    phone: '(720) 375-4270',
+    email: null,
+    image: require('../images/howard_binkley.jpg'),
+  },
+  {
+    name: 'Justin Apfel',
+    role: 'Conservation Director',
+    phone: '(757) 817-4267',
+    email: null,
+    image: require('../images/justin_apfel.jpg'),
+  },
+  {
+    name: 'Cliff Purslow',
+    role: 'Juniors Director',
+    phone: '(720) 980-9090',
+    email: null,
+    image: require('../images/cliff_purslow.jpg'),
+  },
+  {
+    name: 'Bill Cancellieri',
+    role: 'High School Director',
+    phone: '(303) 521-8216',
+    email: null,
+    image: null,
+  },
+];
+
+export default officers;
